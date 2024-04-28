52fa59f5ff41bcbf1590fd3d95721a0ebff0531f
OM <ssplot56@gmail.com>
2024-04-28 19:39:00 +0300

    initial commit

---
+++ src/test/java/project/module/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package project.module.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
